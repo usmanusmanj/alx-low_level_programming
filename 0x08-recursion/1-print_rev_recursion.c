acefc81bfd0213724920c9ced201edd3ee382d9a
usmanusmanjnr <usmanusmanjnr@gmail.com>
2023-04-05 16:18:26 -0700

    0x08. C - Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
-#include "main.h"                                                                                                                                        
-/**                                                                                                                                     
- * _print_rev_recursion - Prints a string in reverse order.                                                                                   
- * @s: The string that will be printed.                                                                                                        
+#include "main.h"
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
  */
 void _print_rev_recursion(char *s)
 {
